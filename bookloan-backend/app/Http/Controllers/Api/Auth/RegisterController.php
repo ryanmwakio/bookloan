58575d9de8f5508cbd2d003834eb8f8221f886ed
Ryan Mwakio <73101551+ryanmwakio@users.noreply.github.com>
2023-12-18 14:46:21 +0300

    (feature) user registration and issuance of token

--- bookloan-backend/app/Http/Controllers/Api/Auth/RegisterController.php
+++ bookloan-backend/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
@@ -13,14 +14,24 @@ class RegisterController extends Controller
      */
     public function __invoke(RegisterRequest $request)
     {
-        $user = User::create($request->getData());
+        try {
+            $user = User::create($request->getData());
 
-        return response()->json([
-            'successful' => true,
-            "message" => "User created successfully",
-            'user' => $user,
-            'token' => $user->createToken('laravel_api_token')->plainTextToken,
-        ]);
+            return response()->json([
+                'successful' => true,
+                "message" => "User created successfully",
+                'user' => $user,
+                'token' => $user->createToken('laravel_api_token')->plainTextToken,
+            ]);
+        } catch (\Exception $e) {
+
+            Log::error($e->getMessage());
+
+            return response()->json([
+                'successful' => false,
+                "message" => $e->getMessage(),
+            ]);
+        }
 
     }
 }
