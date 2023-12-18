0a90afe314711d45435e58d2fe9d307b139f09b3
Ryan Mwakio <73101551+ryanmwakio@users.noreply.github.com>
2023-12-18 16:24:01 +0300

    (feature) created books and book loan controllers

---
+++ bookloan-backend/database/migrations/2023_12_18_121906_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('publisher');
+            $table->string('isbn');
+            $table->string('category');
+            $table->string('sub_category');
+            $table->text('description');
+            $table->integer('pages');
+            $table->text('image');
+            $table->foreignId('added_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
