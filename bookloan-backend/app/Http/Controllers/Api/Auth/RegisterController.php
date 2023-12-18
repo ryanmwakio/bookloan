90872b795ea89a059bc2a0f51f1992e758622bc7
Ryan Mwakio <73101551+ryanmwakio@users.noreply.github.com>
2023-12-18 14:33:08 +0300

    added roles migration to users table

--- bookloan-backend/app/Http/Controllers/Api/Auth/RegisterController.php
+++ bookloan-backend/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(RegisterRequest $request)
     {
-        //
+        $user = User::create($request->getData());
+
+        return response()->json([
+            'successful' => true,
+            "message" => "User created successfully",
+            'user' => $user,
+            'token' => $user->createToken('laravel_api_token')->plainTextToken,
+        ]);
+
     }
 }
