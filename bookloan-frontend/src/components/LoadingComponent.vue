17023eacfaa1f1ae858e86161e301f02170b941d
Ryan Mwakio <73101551+ryanmwakio@users.noreply.github.com>
2023-12-18 13:24:21 +0300

    (feature) option to change the roles on admin

---
+++ bookloan-frontend/src/components/LoadingComponent.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- bookloan-frontend/src/views/AdminUsersView.vue
+++ bookloan-frontend/src/views/AdminUsersView.vue
@@ -1,12 +1,19 @@
 <script setup>
 import AdminNavComponent from "@/components/AdminNavComponent.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <main class="flex gap-4">
     <AdminNavComponent />
     <div class="p-4 my-4">
-      <h2 class="font-semibold text-base">Users</h2>
+      <div>
+        <router-link
+          to="/register"
+          class="bg-primary-orange text-white py-2 px-6 text-xs w-fit h-fit"
+          >Add User</router-link
+        >
+      </div>
       <table class="w-full text-xs mt-4 border">
         <thead class="border">
           <tr class="text-left">
@@ -20,7 +27,20 @@ import AdminNavComponent from "@/components/AdminNavComponent.vue";
           <tr class="border-b py-2">
             <td class="p-2 border">John Doe</td>
             <td class="p-2 border">XZkKzsdsdsdsdsds@example.com</td>
-            <td class="p-2 border">User</td>
+            <td class="p-2 border">
+              <p>User</p>
+              <div class="flex gap-2 items-center">
+                <label for="">change role</label>
+                <select
+                  name=""
+                  id=""
+                  class="border border-primary-black focus:outline-none text-xs cursor-pointer"
+                >
+                  <option value="">Admin</option>
+                  <option value="">User</option>
+                </select>
+              </div>
+            </td>
             <td class="p-2 flex gap-2 border">
               <button class="bg-red-500 text-white py-2 px-6 text-xs w-fit h-fit">Delete</button>
             </td>
@@ -28,7 +48,20 @@ import AdminNavComponent from "@/components/AdminNavComponent.vue";
           <tr class="border-b py-2">
             <td class="p-2 border">John Doe</td>
             <td class="p-2 border">XZkK@example.com</td>
-            <td class="p-2 border">Admin</td>
+            <td class="p-2 border">
+              <p>Admin</p>
+              <div class="flex gap-2 items-center">
+                <label for="">change role</label>
+                <select
+                  name=""
+                  id=""
+                  class="border border-primary-black focus:outline-none text-xs cursor-pointer"
+                >
+                  <option value="">Admin</option>
+                  <option value="">User</option>
+                </select>
+              </div>
+            </td>
             <td class="p-2 flex gap-2 border">
               <button class="bg-red-500 text-white py-2 px-6 text-xs w-fit h-fit">Delete</button>
             </td>
